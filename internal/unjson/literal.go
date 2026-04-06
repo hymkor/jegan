514041ff2a01edd8bbc5c93828f172b8a38468ef
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-04-07 02:16:43 +0900

    Fix: test error at internal/unjson

--- internal/unjson/literal.go
+++ internal/unjson/literal.go
@@ -9,6 +9,7 @@ type Literal struct {
 	json  []byte
 }
 
-func (L *Literal) Value() any     { return L.value }
-func (L *Literal) Json() []byte   { return L.json }
-func (L *Literal) String() string { return fmt.Sprint(L.value) }
+func (L *Literal) Value() any      { return L.value }
+func (L *Literal) Json() []byte    { return L.json }
+func (L *Literal) String() string  { return fmt.Sprint(L.value) }
+func (L Literal) GoString() string { return fmt.Sprint(string(L.json)) }
