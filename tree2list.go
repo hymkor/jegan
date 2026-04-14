3db5e3619ade687475129fda8b832fedfe3808c6
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-04-15 02:18:31 +0900

    Split `read` into `readArray`, `readElements`, `readObject` and `readPairs`

---
+++ tree2list.go
@@ -0,0 +1,97 @@
+package jegan
+
+import (
+	"container/list"
+
+	"github.com/hymkor/jegan/internal/unjson"
+)
+
+func readPairs(prefix []byte, pairs []unjson.KeyValuePair, nest int) *list.List {
+	L := list.New()
+	L.PushBack(newElement(Mark('{'), nest, false, prefix))
+	for _, pair := range pairs {
+		sub := read(pair.Value, nest+1)
+		first := sub.Remove(sub.Front()).(*Element)
+		newFirst := &Pair{
+			spaceKey:   pair.SpaceKey,
+			key:        pair.Key,
+			spaceColon: pair.SpaceColon,
+			Element: Element{
+				spaceValue: pair.Value.SpaceValue,
+				value:      first.value,
+				comma:      first.comma,
+				nest:       nest + 1,
+			},
+		}
+		sub.PushFront(newFirst)
+		node(sub.Back()).SetSpaceCommaOrClose(pair.SpaceCommaOrClose)
+		L.PushBackList(sub)
+	}
+	back := node(L.Back())
+	finalSpace := back.SpaceCommaOrClose()
+	back.SetSpaceCommaOrClose(nil)
+	back.SetComma(false)
+	L.PushBack(newElement(Mark('}'), nest, true, finalSpace))
+	return L
+}
+
+func readObject(prefix []byte, object *unjson.Object, nest int) *list.List {
+	if len(object.Pairs) <= 0 {
+		L := list.New()
+		L.PushBack(newElement(Mark('{'), nest, false, prefix))
+		L.PushBack(newElement(Mark('}'), nest, true, object.Blank))
+		return L
+	}
+	return readPairs(prefix, object.Pairs, nest)
+}
+
+func readElements(prefix []byte, elements []unjson.ArrayElement, nest int) *list.List {
+	L := list.New()
+	L.PushBack(newElement(Mark('['), nest, false, prefix))
+	for _, element := range elements {
+		sub := read(element.Entry, nest+1)
+		node(sub.Back()).SetSpaceCommaOrClose(element.PreComma)
+		L.PushBackList(sub)
+	}
+	back := node(L.Back())
+	finalSpace := back.SpaceCommaOrClose()
+	back.SetSpaceCommaOrClose(nil)
+	back.SetComma(false)
+	L.PushBack(newElement(Mark(']'), nest, true, finalSpace))
+	return L
+}
+
+func readArray(prefix []byte, array *unjson.Array, nest int) *list.List {
+	if len(array.Element) <= 0 {
+		L := list.New()
+		L.PushBack(newElement(Mark('['), nest, false, prefix))
+		L.PushBack(newElement(Mark(']'), nest, true, array.Blank))
+		return L
+	}
+	return readElements(prefix, array.Element, nest)
+}
+
+func read(t *unjson.Entry, nest int) *list.List {
+	v := t.Value
+	prefix := t.SpaceValue
+	if x, ok := v.(*unjson.Object); ok {
+		return readObject(prefix, x, nest)
+	}
+	if x, ok := v.(*unjson.Array); ok {
+		return readArray(prefix, x, nest)
+	}
+	L := list.New()
+	L.PushBack(newElement(v, nest, true, prefix))
+	return L
+}
+
+func Read(v *unjson.Entry) *list.List {
+	if v == nil {
+		return nil
+	}
+	L := read(v, 0)
+	if L != nil && L.Len() > 0 {
+		node(L.Back()).SetComma(false)
+	}
+	return L
+}
