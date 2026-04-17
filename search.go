ac89ac7412ac8ea0bd6c815169eedcdecd74e92a
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-04-17 22:58:19 +0900

    Improve search to match both keys and values

--- search.go
+++ search.go
@@ -1,6 +1,7 @@
 package jegan
 
 import (
+	"container/list"
 	"errors"
 	"fmt"
 	"strings"
@@ -19,11 +20,14 @@ func unwrap(v any) any {
 	return v
 }
 
-func newCompare(v any) (func(any) bool, bool) {
+func newCompare(v any) (func(key string, value any) bool, bool) {
 	v = unwrap(v)
 	if s, ok := v.(string); ok {
-		return func(other any) bool {
-			other = unwrap(other)
+		return func(key string, value any) bool {
+			if strings.Contains(key, s) {
+				return true
+			}
+			other := unwrap(value)
 			if o, ok := other.(string); ok {
 				return strings.Contains(o, s)
 			}
@@ -31,7 +35,7 @@ func newCompare(v any) (func(any) bool, bool) {
 		}, true
 	}
 	if n, ok := v.(float64); ok {
-		return func(other any) bool {
+		return func(_ string, other any) bool {
 			other = unwrap(other)
 			if o, ok := other.(float64); ok {
 				return n == o
@@ -40,13 +44,13 @@ func newCompare(v any) (func(any) bool, bool) {
 		}, true
 	}
 	if v == nil {
-		return func(other any) bool {
+		return func(_ string, other any) bool {
 			other = unwrap(other)
 			return other == nil
 		}, true
 	}
 	if v == true {
-		return func(other any) bool {
+		return func(_ string, other any) bool {
 			other = unwrap(other)
 			if o, ok := other.(bool); ok {
 				return o == true
@@ -55,7 +59,7 @@ func newCompare(v any) (func(any) bool, bool) {
 		}, true
 	}
 	if v == false {
-		return func(other any) bool {
+		return func(_ string, other any) bool {
 			other = unwrap(other)
 			if o, ok := other.(bool); ok {
 				return o == false
@@ -63,7 +67,7 @@ func newCompare(v any) (func(any) bool, bool) {
 			return false
 		}, true
 	}
-	return func(any) bool { return false }, false
+	return func(string, any) bool { return false }, false
 }
 
 func (app *Application) keyFuncSearch(session *pager.Session, revert bool) error {
@@ -109,9 +113,16 @@ func (app *Application) keyFuncSearch(session *pager.Session, revert bool) error
 	return app.searchForward(session, compare)
 }
 
+func compareKeyAndValue(p *list.Element, compare func(string, any) bool) bool {
+	if pair, ok := p.Value.(*Pair); ok {
+		return compare(pair.key, pair.value)
+	}
+	return compare("", ref(p).Value())
+}
+
 func (app *Application) searchForward(
 	session *pager.Session,
-	compare func(any) bool) error {
+	compare func(key string, value any) bool) error {
 
 	_cursor := app.cursor
 	_csrPos := app.csrline
@@ -123,7 +134,7 @@ func (app *Application) searchForward(
 		}
 		_csrPos++
 
-		if compare(ref(_cursor).Value()) {
+		if compareKeyAndValue(_cursor, compare) {
 			app.setCursor(_cursor)
 			app.csrline = _csrPos
 			for _csrPos-session.WinPos >= session.Height {
@@ -138,7 +149,7 @@ func (app *Application) searchForward(
 
 func (app *Application) searchBackward(
 	session *pager.Session,
-	compare func(any) bool) error {
+	compare func(string, any) bool) error {
 
 	_cursor := app.cursor
 	_csrPos := app.csrline
@@ -150,7 +161,7 @@ func (app *Application) searchBackward(
 		}
 		_csrPos--
 
-		if compare(ref(_cursor).Value()) {
+		if compareKeyAndValue(_cursor, compare) {
 			app.setCursor(_cursor)
 			app.csrline = _csrPos
 			for _csrPos < session.WinPos {
