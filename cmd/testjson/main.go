8c757297876363fda8011316a523a77815a20a10
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-04-20 12:44:38 +0900

    cmd/testjson: Support wildcard

--- cmd/testjson/main.go
+++ cmd/testjson/main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
-func main1(r io.Reader, name string) error {
+func check(r io.Reader, name string) error {
 	data, err := io.ReadAll(r)
 	if err != nil {
 		return fmt.Errorf("%s: %w", name, err)
@@ -17,20 +18,40 @@ func main1(r io.Reader, name string) error {
 	if err != nil {
 		return fmt.Errorf("%s: %w", name, err)
 	}
-	fmt.Println("OK")
+	fmt.Println("OK:", name)
 	return nil
 }
 
 func mains(args []string) error {
-	if len(args) > 0 {
-		fd, err := os.Open(args[0])
-		if err != nil {
-			return err
-		}
-		defer fd.Close()
-		return main1(fd, args[0])
+	if len(args) <= 0 {
+		return check(os.Stdin, "<STDIN>")
 	}
-	return main1(os.Stdin, "<STDIN>")
+	for _, arg := range args {
+		if arg == "-" {
+			if err := check(os.Stdin, "<STDIN>"); err != nil {
+				return err
+			}
+			continue
+		}
+		fnames, err := filepath.Glob(arg)
+		if err != nil {
+			fnames = []string{arg}
+		}
+		for _, fname1 := range fnames {
+			fd, err := os.Open(fname1)
+			if err != nil {
+				return err
+			}
+			if err = check(fd, fname1); err != nil {
+				fmt.Println("NG:", err.Error())
+			}
+			err1 := fd.Close()
+			if err1 != nil {
+				return err1
+			}
+		}
+	}
+	return nil
 }
 
 func main() {
