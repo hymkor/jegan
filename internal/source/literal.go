f3f0242b0c396a41f5fca6e0abdb8693f466f02e
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-04-30 15:28:50 +0900

    Remove unused method `.GoString`

--- internal/source/literal.go
+++ internal/source/literal.go
@@ -13,11 +13,10 @@ func NewLiteral(v any, j []byte) *Literal {
 	return &Literal{data: v, json: j}
 }
 
-func (L *Literal) Data() any       { return L.data }
-func (L *Literal) Json() []byte    { return L.json }
-func (L *Literal) String() string  { return fmt.Sprint(L.data) }
-func (L Literal) GoString() string { return fmt.Sprint(string(L.json)) }
-func (L *Literal) Unwrap() any     { return L.data }
+func (L *Literal) Data() any      { return L.data }
+func (L *Literal) Json() []byte   { return L.json }
+func (L *Literal) String() string { return fmt.Sprint(L.data) }
+func (L *Literal) Unwrap() any    { return L.data }
 
 type RawBytes struct {
 	json []byte
@@ -27,7 +26,6 @@ func NewRawBytes(j []byte) *RawBytes {
 	return &RawBytes{json: j}
 }
 
-func (R *RawBytes) Data() any       { return string(R.json) }
-func (R *RawBytes) Json() []byte    { return R.json }
-func (R *RawBytes) String() string  { return string(R.json) }
-func (R RawBytes) GoString() string { return string(R.json) }
+func (R *RawBytes) Data() any      { return string(R.json) }
+func (R *RawBytes) Json() []byte   { return R.json }
+func (R *RawBytes) String() string { return string(R.json) }
